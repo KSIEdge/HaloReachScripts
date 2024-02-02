95d52f4db14c7fe25244d1df76a15993f4cd6b72
Nick <46254507+KSIEdge@users.noreply.github.com>
2024-02-02 14:19:41 -0700

    Create Phantom Parameters.c

---
+++ Firefight/Phantom Scripts/Phantom Parameters.c
@@ -0,0 +1,25 @@
+; ===================================================================
+	; phantom parameters ================================================
+	; ===================================================================
+	
+	
+		; assign phantom squads to global ai variables 
+			(set ai_sur_phantom_01 sq_sur_phantom_01) // set ai to phantom in game
+			(set ai_sur_phantom_02 sq_sur_phantom_02)
+			(set ai_sur_phantom_03 sq_sur_phantom_03)
+			(set ai_sur_phantom_04 sq_sur_phantom_04)
+	
+			; set phantom load parameters 
+				(set s_sur_drop_side_01 "right") //right = right side opens, left = left side opens, dual = both sides open
+				(set s_sur_drop_side_02 "left")
+				(set s_sur_drop_side_03 "dual")
+				(set s_sur_drop_side_04 "dual")
+				;(set b_sur_dropship_r1_initial true) //initialize
+				;(set b_sur_dropship_r2_initial true)
+				;(set b_sur_dropship_r3_initial true)
+				;(set b_sur_dropship_r1_primary true) //utilize by firefight as dropship
+				;(set b_sur_dropship_r2_primary true)
+				;(set b_sur_dropship_r3_primary true)
+				;(set b_sur_dropship_r1_boss true) //its a boss
+				;(set b_sur_dropship_r2_boss true)
+				;(set b_sur_dropship_r3_boss true)
